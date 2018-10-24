1508066bc6e76a6e719df8c221ae548e883305a9
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-10-25 00:42:49 +0800

    Fix bug

--- train.py
+++ train.py
@@ -18,70 +18,90 @@ from six.moves import urllib
 # Sets the logging level for TensorFlow library.
 tf.logging.set_verbosity(tf.logging.INFO)
 
-RESIZE_HEIGHT = 28
-RESIZE_WIDTH = 28
+RESIZE_HEIGHT = 128
+RESIZE_WIDTH = 128
+CHANNEL = 1
+
+LOGGING_NAME = "sigmoid_tensor"
 
 # Model function for CNN.
 def cnn_model_fn(features, labels, mode):
 	labels = tf.reshape(labels, [1, 1])
 
 	# Input Layer
-	# Re-shapes the input to [batch_size = -1, width, height, channels = 1]
-	input_layer = tf.reshape(features, [-1, RESIZE_HEIGHT, RESIZE_WIDTH, 1])
+	# Re-shapes the input to [batch_size = -1, width, height, channel]
+	input_layer = tf.reshape(features, [-1, RESIZE_HEIGHT, RESIZE_WIDTH, CHANNEL])
+	print("The input layer size is %s" % input_layer.shape)
 
 	# Convolutional Layer #1
-	# Uses a kernel of size 5*5 to extract 32 features.
+	# Uses a kernel of size 5*5 to extract 32 features (the dimension of the output space).
 	# Uses ReLU as the activation function.
+	# [RESIZE_HEIGHT, RESIZE_WIDTH, CHANNEL] => [RESIZE_HEIGHT, RESIZE_WIDTH, 32]
 	conv1 = tf.layers.conv2d(
 		inputs=input_layer,
 		filters=32,
 		kernel_size=[5, 5],
 		padding="same",
 		activation=tf.nn.relu)
+	print("The conv1 layer size is %s" % conv1.shape)
 
 	# Pooling Layer #1
 	# Uses a max-pooling layer with pool size 2*2.
+	# [RESIZE_HEIGHT, RESIZE_WIDTH, 32] => [RESIZE_HEIGHT / 2, RESIZE_WIDTH / 2, 32]
 	pool1 = tf.layers.max_pooling2d(
 		inputs=conv1,
 		pool_size=[2, 2],
 		strides=2)
+	print("The pool1 layer size is %s" % pool1.shape)
 
 	# Convolutional Layer #2
+	# Uses a kernel of size 5*5 to extract 32 features (the dimension of the output space).
+	# Uses ReLU as the activation function.
+	# [RESIZE_HEIGHT / 2, RESIZE_WIDTH / 2, 32] => [RESIZE_HEIGHT / 2, RESIZE_WIDTH / 2, 32]
 	conv2 = tf.layers.conv2d(
 		inputs=pool1,
-		filters=64,
+		filters=32,
 		kernel_size=[5, 5],
 		padding="same",
 		activation=tf.nn.relu)
+	print("The conv2 layer size is %s" % conv2.shape)
 
 	# Pooling Layer #2
+	# [RESIZE_HEIGHT / 2, RESIZE_WIDTH / 2, 32] => [RESIZE_HEIGHT / 4, RESIZE_WIDTH / 4, 32]
 	pool2 = tf.layers.max_pooling2d(
 		inputs=conv2,
 		pool_size=[2, 2],
 		strides=2)
+	print("The pool2 layer size is %s" % pool2.shape)
 
-	# Dense Layer #1
-	pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-	dense      = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-	dropout    = tf.layers.dropout(inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+	# Flats the tensor into a batch of vectors
+	pool2_flat = tf.reshape(pool2, [-1, int(RESIZE_HEIGHT / 4) * int(RESIZE_HEIGHT / 4) * 32])
+	print("The flatten pool2 size is %s" % pool2_flat.shape)
 
-	# Dense Layer #2 (Logits Layer)
+	# Dense (fully connected) Layer #1
+	dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+
+	# Performs dropout operation here as a regularization technique.
+	dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=(mode == tf.estimator.ModeKeys.TRAIN))
+
+	# Dense (fully connected) Layer #2 (Logits Layer)
 	logits = tf.layers.dense(inputs=dropout, units=1)
 
 	predictions = {
 	  # Generate predictions (for PREDICT and EVAL mode)
 	  # "classes": tf.argmax(input=logits, axis=1),
 	  # Add `softmax_tensor` to the graph. It is used for PREDICT and by the `logging_hook`.
-	  "probabilities": tf.nn.sigmoid(logits, name="sigmoid_tensor")
-	  }
+	  "probabilities": tf.nn.sigmoid(logits, name=LOGGING_NAME)
+	}
 
+	# Returns the prediction estimator (for PREDICT mode)
 	if mode == tf.estimator.ModeKeys.PREDICT:
 	  	return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-	# Calculate Loss (for both TRAIN and EVAL modes)
+	# Calculates Loss (for both TRAIN and EVAL modes)
 	loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=labels, logits=logits)
 
-	# Configure the Training Op (for TRAIN mode)
+	# Configures the Training operation (for TRAIN mode)
 	if mode == tf.estimator.ModeKeys.TRAIN:
 		optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
 		train_op = optimizer.minimize(
@@ -90,10 +110,10 @@ def cnn_model_fn(features, labels, mode):
 		return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
 	# Add evaluation metrics (for EVAL mode)
-	eval_metric_ops = {
-	"accuracy": tf.metrics.accuracy(
-		labels=labels, predictions=tf.round(predictions["probabilities"]))}
+	eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels=labels,
+													   predictions=tf.round(predictions["probabilities"]))}
 
+	# Returns the evaluation estimator (for EVAL mode)
 	return tf.estimator.EstimatorSpec(
 		mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
@@ -109,10 +129,10 @@ def loadJson(path):
 
 def readAndResizeImageToTensor(imagePath):
     file = tf.read_file(imagePath)
-    decoded = tf.image.decode_jpeg(file, channels=1)
+    decoded = tf.image.decode_jpeg(file, channels=CHANNEL)
     return tf.image.resize_images(decoded, [RESIZE_HEIGHT, RESIZE_WIDTH])
 
-def createDataset(prefix, images, size_limit=1000):
+def createDataset(prefix, images, size_limit=100):
 	files = []
 	labels = []
 
@@ -170,7 +190,7 @@ def main(argv):
 		return createDataset("test/", images2)
 
 	# Set up logging for predictions
-	tensors_to_log = {"probabilities": "sigmoid_tensor"}
+	tensors_to_log = {"probabilities": LOGGING_NAME}
 	logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=100)
 
 	# Create the Estimator
