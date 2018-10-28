6ab60e67fe6d18ba864c92c3648a59fa64f5d163
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-10-28 23:14:03 +0800

    Add dropout layer

--- convolution_net.py
+++ convolution_net.py
@@ -9,22 +9,25 @@ class ConvolutionNet(nn.Module):
         super(ConvolutionNet, self).__init__()
 
         # Input channels = 3 (32 * 32), output channels = 18 (32 * 32)
-        self.conv1 = torch.nn.Conv2d(3, 18, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(3, 18, kernel_size=3, stride=1, padding=1)
 
         # Input channels = 18 (32 * 32), output channels = 18 (16 * 16)
-        self.pool1 = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         # Input channels = 18 (16 * 16), output channels = 18 (16 * 16)
-        self.conv2 = torch.nn.Conv2d(18, 18, kernel_size=5, stride=1, padding=2)
+        self.conv2 = nn.Conv2d(18, 18, kernel_size=5, stride=1, padding=2)
 
         # Input channels = 18 (16 * 16), output channels = 18 (8 * 8)
-        self.pool2 = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         # 1152 (18 * 8 * 8) input features, 64 output features (see sizing flow below).
-        self.fc1 = torch.nn.Linear(18 * 8 * 8, 64)
+        self.fc1 = nn.Linear(18 * 8 * 8, 64)
+
+        # Dropout layer: drops with probability 0.4
+        self.dropout = nn.Dropout(p=0.4)
 
         # 64 input features, 10 output features for our 10 defined classes
-        self.fc2 = torch.nn.Linear(64, 2)
+        self.fc2 = nn.Linear(64, 2)
 
     def forward(self, x):
         # conv1: from (3, 32, 32) to (18, 32, 32)
@@ -39,5 +42,7 @@ class ConvolutionNet(nn.Module):
         x = x.view(-1, 18 * 8 * 8)
         # fc1: from 1152 (18 * 8 * 8) to 64
         x = F.relu(self.fc1(x))
+        # dropout: p = 0.4
+        x = self.dropout(x)
         # fc2: from 64 to 2
         return self.fc2(x)
