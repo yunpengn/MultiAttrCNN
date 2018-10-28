36207dd3b75e0f0e63f7a9c518361b6cdc470eee
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-10-28 15:00:12 +0800

    Add extract script for LFW

---
+++ extract_lfw.py
@@ -0,0 +1,42 @@
+import json
+import numpy as np
+import os
+import shutil
+import sys
+import tensorflow as tf
+import traceback
+
+data_dir = "LFW_data"
+extract_dir = "LFW_extract"
+label_dir = "LFW_label"
+label_male_names = "male_names.txt"
+label_female_names = "female_names.txt"
+
+def convert_name_to_path(name):
+	# Split by last occurrence of "_"
+	split = name.rsplit("_", 1)
+	return os.path.join(data_dir, split[0], name)
+
+def copy_file(src, dest):
+	shutil.copyfile(src, dest)
+
+def extract_gender(label_file, extract_train_folder, extract_val_folder):
+	with open(label_file) as f:
+		content = [line.strip().rstrip('\n') for line in f.readlines()]
+		print("Have read %s lines from file at %s." % (len(content), label_file))
+
+		for line in content:
+			image_path = convert_name_to_path(line)
+			if (not os.path.isfile(image_path)):
+				print("WARNING: the image at %s does not exist." % image_path)
+				continue
+
+			dest_path = os.path.join(extract_train_folder, line)
+			copy_file(image_path, dest_path)
+
+		print("Finished the processing on the file at %s." % label_file)
+
+extract_gender(os.path.join(label_dir, label_male_names),
+			   os.path.join(extract_dir, "train_male"), os.path.join(extract_dir, "val_male"))
+extract_gender(os.path.join(label_dir, label_female_names),
+			   os.path.join(extract_dir, "train_female"), os.path.join(extract_dir, "val_female"))
